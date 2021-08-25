2d47a7881abca050f7c0ae4e25cb49da24d36e00
Azeez <azeezshahrizal@gmail.com>
2021-08-26 00:36:43 +0800

    basic variable

---
+++ Py Fundamentals/basicVariable.py
@@ -0,0 +1,32 @@
+"""
+Variable Name
+myvar   = "Azeez"
+my_var  = "Azeez"
+_my_var = "Azeez"
+myVar   = "Azeez"
+MYVAR   = "Azeez"
+myvar2  = "Azeez"
+
+Camel Case - Each word, except the first, starts with a capital letter.
+myVariableName
+
+Pascal Case - Each word, starts with a capital letter.
+MyVariableName
+
+Snake Case - Each word seperated by an underscore
+my_variable_name
+"""
+
+a, b, c = "Alpha", "Bravo", "Charlie"
+d = e = f = "Delta"
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+fruits = ["apple", "banana", "grapes"]
+g, h, i = fruits
+print(g)
+

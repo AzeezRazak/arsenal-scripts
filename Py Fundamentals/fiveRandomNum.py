ef664d05e83958934c29aa27e849f1871bf8b251
Azeez <azeezshahrizal@gmail.com>
2022-02-08 17:47:09 +0800

    adjust script to include a delay, to reduce disk usage for first test

--- Py Fundamentals/fiveRandomNum.py
+++ Py Fundamentals/fiveRandomNum.py
@@ -1,5 +1,5 @@
 from random import randint
-import datetime
+import datetime, time
 import csv
 import pandas as pd
 
@@ -7,51 +7,27 @@ def generateInteger():
     value = randint(0,500)
     return(value)
 
-now = datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
-"""
-df=pd.DataFrame(
-    columns=['time','intVar1','intVar2','intVar3', 'intVar4']
-)
-"""
+def generateNow():
+    now = datetime.datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+    return(now)
+
 df = pd.DataFrame({
-    "time": now,
+    "time": generateNow(),
     "intVar1": [generateInteger()],
     "intVar2": [generateInteger()],
     "intVar3": [generateInteger()],
     "intVar4": [generateInteger()],
     })
-df.to_csv('Names.csv')
+df.to_csv('Names.csv', index=False)
 
 while(1):
     df1 = pd.DataFrame({
-    "time": now,
+    "time": generateNow(),
     "intVar1": [generateInteger()],
     "intVar2": [generateInteger()],
     "intVar3": [generateInteger()],
     "intVar4": [generateInteger()],
     })
-    df1.to_csv('Names.csv', mode='a', header=False)
+    df1.to_csv('Names.csv', mode='a', header=False, index=False)
+    time.sleep(15)
 
-
-"""
-randomNumberDict = {
-    "time": now,
-    "intVar1": generateInteger(),
-    "intVar2": generateInteger(),
-    "intVar3": generateInteger(),
-    "intVar4": generateInteger(),
-}
-
-randomNumberDict={}
-df = pd.DataFrame(randomNumberDict, columns= ['time', 'intVar1', 'intVar2', 'intVar3', 'intVar4'])
-while(1):
-    randomNumberDict = {
-    "time": now,
-    "intVar1": generateInteger(),
-    "intVar2": generateInteger(),
-    "intVar3": generateInteger(),
-    "intVar4": generateInteger(),
-    }
-    df.to_csv('Names.csv', index=False)
-#df = pd.DataFrame(randomNumberDict, columns= ['time', 'intVar1', 'intVar2', 'intVar3', 'intVar4'])
-"""
